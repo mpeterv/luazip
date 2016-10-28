9446ac060c4253da476b5cf3c22c93757519ed07
Peter Melnichenko <mpeterval@gmail.com>
2016-10-28 19:44:56 +0300

    Apply patch from rockspec

--- src/luazip.c
+++ src/luazip.c
@@ -21,6 +21,42 @@
 #define ZIPINTERNALFILEHANDLE  "lzipInternalFile"
 #define LUAZIP_MAX_EXTENSIONS 32
 
+#ifndef luaL_reg
+#define luaL_reg luaL_Reg
+#endif
+#ifndef luaL_getn
+#define luaL_getn luaL_len
+#endif
+#ifndef lua_strlen
+#define lua_strlen luaL_len
+#endif
+#ifndef luaL_optlong
+#define luaL_optlong luaL_optinteger
+#endif
+#if LUA_VERSION_NUM >= 501
+#if LUA_VERSION_NUM == 501
+/* From https://github.com/keplerproject/lua-compat-5.2/blob/v0.3/c-api/compat-5.2.c */
+void luaL_setfuncs (lua_State *L, const luaL_Reg *l, int nup) {
+  luaL_checkstack(L, nup+1, "too many upvalues");
+  for (; l->name != NULL; l++) {  /* fill the table with given functions */
+    int i;
+    lua_pushstring(L, l->name);
+    for (i = 0; i < nup; i++)  /* copy upvalues to the top */
+      lua_pushvalue(L, -(nup + 1));
+    lua_pushcclosure(L, l->func, nup);  /* closure with those upvalues */
+    lua_settable(L, -(nup + 3)); /* table must be below the upvalues, the name and the closure */
+  }
+  lua_pop(L, nup);  /* remove upvalues */
+}
+#endif
+#ifndef LUA_COMPAT_OPENLIB
+static void luaL_openlib(lua_State *L, const char* name, const luaL_Reg* lib, int nup) {
+  lua_newtable(L); luaL_setfuncs(L, lib, nup);
+  if (name) { lua_pushvalue(L, -1); lua_setglobal(L, name); }
+}
+#endif
+#endif
+
 static int pushresult (lua_State *L, int i, const char *filename) {
   if (i) {
     lua_pushboolean(L, 1);
