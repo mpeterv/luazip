863ab18f0087348191ab52d84c6a03acee89da26
Peter Melnichenko <mpeterval@gmail.com>
2016-11-04 20:18:18 +0300

    Run file tests both on real files and files inside archive
    Also remove a test that fails on real files: :seek() beyond file end
    doesn't error, as underlying lseek() doesn't.

--- spec/zip_spec.lua
+++ spec/zip_spec.lua
@@ -145,213 +145,229 @@ describe("zip file object", function()
 end)
 
 describe("file object", function()
-   local zfile
-   local file
+   local function register_tests(name, get_file, clean_up)
+      local file
 
-   before_each(function()
+      before_each(function()
+         file = get_file()
+      end)
+
+      after_each(function()
+         if file then
+            file:close()
+         end
+
+         if clean_up then
+            clean_up()
+         end
+      end)
+
+      describe(name, function()
+         describe(":read", function()
+            it("throws an error on invalid mode", function()
+               assert.has_error(function() file:read("*x") end, "bad argument #1 to 'read' (invalid format)")
+            end)
+
+            it("reads whole file", function()
+               local str = file:read("*a")
+               assert.is_equal(([[
+
+LuaZip is a lightweight Lua extension library used to read files stored inside zip files.
+Please see docs at doc/index.html or http://luazip.luaforge.net/
+]]):gsub("\n", "\r\n"), str)
+            end)
+
+            it("reads lines", function()
+               local str = file:read("*l")
+               assert.is_equal("\r", str)
+               str = file:read("*l")
+               assert.is_equal("LuaZip is a lightweight Lua extension library used to read files stored inside zip files.\r", str)
+            end)
+
+            it("reads lines by default", function()
+               local str = file:read()
+               assert.is_equal("\r", str)
+               str = file:read()
+               assert.is_equal("LuaZip is a lightweight Lua extension library used to read files stored inside zip files.\r", str)
+            end)
+
+            it("reads given number of bytes", function()
+               local str = file:read(2)
+               assert.is_equal("\r\n", str)
+               str = file:read(3)
+               assert.is_equal("Lua", str)
+            end)
+
+            it("returns nil on EOF when reading given number of bytes", function()
+               local str = file:read(1000)
+               assert.is_equal(([[
+
+LuaZip is a lightweight Lua extension library used to read files stored inside zip files.
+Please see docs at doc/index.html or http://luazip.luaforge.net/
+]]):gsub("\n", "\r\n"), str)
+               str = file:read(1)
+               assert.is_nil(str)
+            end)
+
+            it("returns nil on EOF and empty string otherwise when reading 0 bytes", function()
+               local str = file:read(0)
+               assert.is_equal("", str)
+               str = file:read(2)
+               assert.is_equal("\r\n", str)
+               file:read(1000)
+               str = file:read(0)
+               assert.is_nil(str)
+               str = file:read(1)
+               assert.is_nil(str)
+            end)
+
+            it("returns nil on EOF when reading a line", function()
+               file:read(1000)
+               local str = file:read("*l")
+               assert.is_nil(str)
+               str = file:read()
+               assert.is_nil(str)
+            end)
+
+            it("returns an empty string on EOF when reading whole file", function()
+               file:read(1000)
+               local str = file:read("*a")
+               assert.is_equal("", str)
+            end)
+
+            it("accepts several modes", function()
+               local str1, str2, str3 = file:read("*l", 6, "*l")
+               assert.is_equal("\r", str1)
+               assert.is_equal("LuaZip", str2)
+               assert.is_equal(" is a lightweight Lua extension library used to read files stored inside zip files.\r", str3)
+            end)
+         end)
+
+         describe(":seek", function()
+            it("gets current position without changing it by default", function()
+               local pos = file:seek()
+               assert.is_equal(0, pos)
+               file:read(5)
+               pos = file:seek()
+               assert.is_equal(5, pos)
+            end)
+
+            it("moves position to file start with set by default", function()
+               local str1 = file:read(10)
+               local pos = file:seek("set")
+               assert.is_equal(0, pos)
+               local str2 = file:read(10)
+               assert.are_equal(str1, str2)
+            end)
+
+            it("moves position relatively to file start with set", function()
+               local pos = file:seek("set", 14)
+               assert.is_equal(14, pos)
+               local str = file:read(11)
+               assert.is_equal("lightweight", str)
+            end)
+
+            it("gets current position without changing it with cur by default", function()
+               local pos = file:seek("cur")
+               assert.is_equal(0, pos)
+               file:read(5)
+               pos = file:seek("cur")
+               assert.is_equal(5, pos)
+            end)
+
+            it("moves position relatively to current position with cur", function()
+               file:seek("set", 10)
+               local pos = file:seek("cur", 4)
+               assert.is_equal(14, pos)
+               local str = file:read(11)
+               assert.is_equal("lightweight", str)
+               pos = file:seek()
+               assert.is_equal(25, pos)
+               pos = file:seek("cur", -14)
+               assert.is_equal(11, pos)
+            end)
+
+            it("moves position to file end with end by default", function()
+               file:seek("set", 10)
+               local pos = file:seek("cur", 4)
+               assert.is_equal(14, pos)
+               local str = file:read(11)
+               assert.is_equal("lightweight", str)
+               pos = file:seek()
+               assert.is_equal(25, pos)
+               pos = file:seek("cur", -14)
+               assert.is_equal(11, pos)
+            end)
+
+            it("moves position relatively to file with end", function()
+               local pos = file:seek("end", -29)
+               assert.is_equal(130, pos)
+               local str = file:read("*l")
+               assert.is_equal("http://luazip.luaforge.net/\r", str)
+            end)
+         end)
+
+         describe(":lines", function()
+            it("returns an iterator reading lines from the file", function()
+               local iter = file:lines()
+               assert.is_function(iter)
+               local lines = {}
+
+               for line in iter do
+                  table.insert(lines, line)
+               end
+
+               assert.is_same({
+                  "\r",
+                  "LuaZip is a lightweight Lua extension library used to read files stored inside zip files.\r",
+                  "Please see docs at doc/index.html or http://luazip.luaforge.net/\r"
+               }, lines)
+            end)
+         end)
+
+         describe("__tostring", function()
+            it("returns 'file in zip file (address) on an open file", function()
+               local str = tostring(file)
+               assert.matches("^file in zip file %(.+%)$", str)
+            end)
+
+            it("returns 'file in zip file (closed) on a closed file", function()
+               file:close()
+               local str = tostring(file)
+               file = nil
+               assert.is_equal("file in zip file (closed)", str)
+            end)
+         end)
+
+         describe(":close", function()
+            it("returns true on success", function()
+               local ok = file:close()
+               file = nil
+               assert.is_true(ok)
+            end)
+         end)
+      end)
+   end
+
+   local zfile
+
+   register_tests("(in zip archive)", function()
       local err
       zfile, err = zip.open("spec/luazip.zip")
       assert.is_truthy(zfile, err)
+      local file
       file, err = zfile:open("README")
       assert.is_truthy(file, err)
-   end)
-
-   after_each(function()
-      if file then
-         file:close()
-      end
-
+      return file
+   end, function()
       if zfile then
          zfile:close()
       end
    end)
 
-   describe(":read", function()
-      it("throws an error on invalid mode", function()
-         assert.has_error(function() file:read("*x") end, "bad argument #1 to 'read' (invalid format)")
-      end)
-
-      it("reads whole file", function()
-         local str = file:read("*a")
-         assert.is_equal(([[
-
-LuaZip is a lightweight Lua extension library used to read files stored inside zip files.
-Please see docs at doc/index.html or http://luazip.luaforge.net/
-]]):gsub("\n", "\r\n"), str)
-      end)
-
-      it("reads lines", function()
-         local str = file:read("*l")
-         assert.is_equal("\r", str)
-         str = file:read("*l")
-         assert.is_equal("LuaZip is a lightweight Lua extension library used to read files stored inside zip files.\r", str)
-      end)
-
-      it("reads lines by default", function()
-         local str = file:read()
-         assert.is_equal("\r", str)
-         str = file:read()
-         assert.is_equal("LuaZip is a lightweight Lua extension library used to read files stored inside zip files.\r", str)
-      end)
-
-      it("reads given number of bytes", function()
-         local str = file:read(2)
-         assert.is_equal("\r\n", str)
-         str = file:read(3)
-         assert.is_equal("Lua", str)
-      end)
-
-      it("returns nil on EOF when reading given number of bytes", function()
-         local str = file:read(1000)
-         assert.is_equal(([[
-
-LuaZip is a lightweight Lua extension library used to read files stored inside zip files.
-Please see docs at doc/index.html or http://luazip.luaforge.net/
-]]):gsub("\n", "\r\n"), str)
-         str = file:read(1)
-         assert.is_nil(str)
-      end)
-
-      it("returns nil on EOF and empty string otherwise when reading 0 bytes", function()
-         local str = file:read(0)
-         assert.is_equal("", str)
-         str = file:read(2)
-         assert.is_equal("\r\n", str)
-         file:read(1000)
-         str = file:read(0)
-         assert.is_nil(str)
-         str = file:read(1)
-         assert.is_nil(str)
-      end)
-
-      it("returns nil on EOF when reading a line", function()
-         file:read(1000)
-         local str = file:read("*l")
-         assert.is_nil(str)
-         str = file:read()
-         assert.is_nil(str)
-      end)
-
-      it("returns an empty string on EOF when reading whole file", function()
-         file:read(1000)
-         local str = file:read("*a")
-         assert.is_equal("", str)
-      end)
-
-      it("accepts several modes", function()
-         local str1, str2, str3 = file:read("*l", 6, "*l")
-         assert.is_equal("\r", str1)
-         assert.is_equal("LuaZip", str2)
-         assert.is_equal(" is a lightweight Lua extension library used to read files stored inside zip files.\r", str3)
-      end)
-   end)
-
-   describe(":seek", function()
-      it("gets current position without changing it by default", function()
-         local pos = file:seek()
-         assert.is_equal(0, pos)
-         file:read(5)
-         pos = file:seek()
-         assert.is_equal(5, pos)
-      end)
-
-      it("moves position to file start with set by default", function()
-         local str1 = file:read(10)
-         local pos = file:seek("set")
-         assert.is_equal(0, pos)
-         local str2 = file:read(10)
-         assert.are_equal(str1, str2)
-      end)
-
-      it("moves position relatively to file start with set", function()
-         local pos = file:seek("set", 14)
-         assert.is_equal(14, pos)
-         local str = file:read(11)
-         assert.is_equal("lightweight", str)
-      end)
-
-      it("returns nil when new position is out of range", function()
-         local pos = file:seek("set", 1000)
-         assert.is_nil(pos)
-      end)
-
-      it("gets current position without changing it with cur by default", function()
-         local pos = file:seek("cur")
-         assert.is_equal(0, pos)
-         file:read(5)
-         pos = file:seek("cur")
-         assert.is_equal(5, pos)
-      end)
-
-      it("moves position relatively to current position with cur", function()
-         file:seek("set", 10)
-         local pos = file:seek("cur", 4)
-         assert.is_equal(14, pos)
-         local str = file:read(11)
-         assert.is_equal("lightweight", str)
-         pos = file:seek()
-         assert.is_equal(25, pos)
-         pos = file:seek("cur", -14)
-         assert.is_equal(11, pos)
-      end)
-
-      it("moves position to file end with end by default", function()
-         file:seek("set", 10)
-         local pos = file:seek("cur", 4)
-         assert.is_equal(14, pos)
-         local str = file:read(11)
-         assert.is_equal("lightweight", str)
-         pos = file:seek()
-         assert.is_equal(25, pos)
-         pos = file:seek("cur", -14)
-         assert.is_equal(11, pos)
-      end)
-
-      it("moves position relatively to file with end", function()
-         local pos = file:seek("end", -29)
-         assert.is_equal(130, pos)
-         local str = file:read("*l")
-         assert.is_equal("http://luazip.luaforge.net/\r", str)
-      end)
-   end)
-
-   describe(":lines", function()
-      it("returns an iterator reading lines from the file", function()
-         local iter = file:lines()
-         assert.is_function(iter)
-         local lines = {}
-
-         for line in iter do
-            table.insert(lines, line)
-         end
-
-         assert.is_same({
-            "\r",
-            "LuaZip is a lightweight Lua extension library used to read files stored inside zip files.\r",
-            "Please see docs at doc/index.html or http://luazip.luaforge.net/\r"
-         }, lines)
-      end)
-   end)
-
-   describe("__tostring", function()
-      it("returns 'file in zip file (address) on an open file", function()
-         local str = tostring(file)
-         assert.matches("^file in zip file %(.+%)$", str)
-      end)
-
-      it("returns 'file in zip file (closed) on a closed file", function()
-         file:close()
-         local str = tostring(file)
-         file = nil
-         assert.is_equal("file in zip file (closed)", str)
-      end)
-   end)
-
-   describe(":close", function()
-      it("returns true on success", function()
-         local ok = file:close()
-         file = nil
-         assert.is_true(ok)
-      end)
+   register_tests("(real)", function()
+      local file, err = zip.openfile("spec/readme_sample")
+      assert.is_truthy(file, err)
+      return file
    end)
 end)
